5bde7f2b2f0603345b7d44701c996f426d4d972b
nabiljaved <nabeeljaved2016@gmail.com>
2021-08-04 03:29:32 -0700

    flex-box

--- App.js
+++ App.js
@@ -1,64 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, FlatList, View, Alert} from 'react-native'
-import { useState } from 'react'
-import Header from './components/header'
-import ToDoItem from './components/todoItem'
-import AddToDo from './components/addtodo.js'
-export default function App() {
+import { StyleSheet, Text, View} from 'react-native'
 
-  const [todos, setTodos] = useState([
 
-    {text:'buy coffee for me', key:'1'},
-    {text:'create an app for me', key:'2'},
-    {text:'play on the beach', key:'3'}
-  ])
 
-  const pressHandler = (id) => {
-      setTodos(prevTodos => prevTodos.filter(todo => todo.key != id))
-  }
-
-  const addToDo = (todo) => {
-
-      if(todo.length > 3){
-        setTodos(prevpeople => [{text: todo, key:Math.random().toString()}, ...prevpeople])
-      }else{
-        Alert.alert('Warning', 'Todo Must be over 3 character long', [{text:'Understood', onPress: () => console.log('alert changed')}])  
-      }
-  }
-
-  
+export default function App() {  
   return (
     
     <View style={styles.container}>  
-      <Header/>
-      <View style={styles.content}>
-      {/*to do form*/}
-      <AddToDo addToDo={addToDo}/>
-      <View style={styles.list}>
-        <FlatList 
-          data={todos}
-          renderItem={({item}) => <ToDoItem item={item} pressHandler={pressHandler}/>}               
-        />
-      </View>
-      </View>
+        <Text style={styles.boxOne}>One</Text>
+        <Text style={styles.boxTwo}>Two</Text>
+        <Text style={styles.boxThree}>Three</Text>
+        <Text style={styles.boxFour}>Four </Text>
     </View>
     
   );
 }
 
 const styles = StyleSheet.create({
-  container: {  
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop:40,
-    paddingHorizontal:20
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  content:{
-    padding:40
+  
+  container:{
+    paddingTop:40,    
+    backgroundColor: '#ddd',
+    flexDirection:'row',
+    justifyContent: 'space-around',
+     alignItems: 'flex-end'
+    // flex:1
   }, 
-  list:{
+  boxOne:{
+    flex:1,
+    backgroundColor:'violet',
+    padding:10
+  },
+  boxTwo:{
+    flex:2,
+    backgroundColor:'gold',
+    padding:20
+  },
+  boxThree:{
+    flex:1,
+    backgroundColor:'coral',
     padding:30
+  },
+  
+  boxFour:{
+    flex:4,
+    backgroundColor:'skyblue',
+    padding:40
   }
+
 });
